2d413e397ba081faefe7c95447ec303544f4b7c8
Mahalingam <sundarraj.mahalingam@capgemini.com>
2024-05-18 17:25:21 +0530

    pushed by sundarraj

---
+++ src/com/autoboxing/AutoboxingDemo.java
@@ -0,0 +1,16 @@
+package com.autoboxing;
+
+public class AutoboxingDemo {
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		
+		//converting int -- Integer
+		Integer obj1 = Integer.valueOf(a);
+		
+		System.out.println(obj1);
+
+	}
+
+}
---
+++ src/com/unboxing/UnboxingDemo.java
@@ -0,0 +1,16 @@
+package com.unboxing;
+
+public class UnboxingDemo {
+
+	public static void main(String[] args) {
+		
+		Integer in = 12;
+		
+		//Converting Integer -- int
+		int i = in;
+		
+		System.out.println(i);
+
+	}
+
+}
